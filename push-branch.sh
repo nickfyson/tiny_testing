e59bfcb7bc0edfcb4382db91434679a0996945ce
nickfyson <nickfyson@github.com>
2022-05-26 13:39:14 +0100

    changes for an experiment

---
+++ push-branch.sh
@@ -0,0 +1,6 @@
+
+OLD_HEAD=$(git rev-parse HEAD)
+git add .
+git commit -m 'changes for an experiment'
+git push origin `git rev-parse HEAD`:refs/heads/experiments/`echo $USER`/`date +"%Y%m%d%H%M%S"`
+git reset $OLD_HEAD
